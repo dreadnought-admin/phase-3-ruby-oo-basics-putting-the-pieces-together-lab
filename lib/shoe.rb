2bf450a4c0c2424f1377526e871fe1a80993f74a
dom alexander <dom.alexan@gmail.com>
2022-12-19 11:29:30 -0700

    commit tree f9f23e4a59a1e3c63dc4d822e3b23c8531c68e84

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+require 'pry'
+
+class Shoe 
+    attr_reader :brand 
+    attr_accessor :condition, :color, :size, :material
+
+    def initialize(brand)
+        @brand = brand
+    end 
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end 
+
+end 
+
